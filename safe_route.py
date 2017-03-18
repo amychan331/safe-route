2781d3cae28b27472fb365042ac6bf697d2050a5
Magdalena Strzoda <magdalena.strzoda@myeatclub.com>
2017-03-18 13:31:30 -0700

    Added google maps example

--- safe_route.py
+++ safe_route.py
@@ -7,13 +7,19 @@ Created on Sat Mar 18 13:01:30 2017
 
 
 from flask import Flask
- 
+from flask import send_from_directory
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
     return "Welcome!"
+
+
+@app.route("/example")
+def gmaps_example():
+    return send_from_directory('static/html', 'gmaps-example.html')
 
 
 if __name__ == "__main__":
---
+++ static/js/map.js
@@ -0,0 +1,3 @@
+/**
+ * Created by mstrzoda on 3/18/17.
+ */
