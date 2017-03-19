05c6d960cbe5c59e2f3dd0bf8bd2b5d1efccf960
Anju Mercian <amalphonse@users.noreply.github.com>
2017-03-18 17:08:57 -0700

    updated safe.py with render to feedback form

--- safe_route.py
+++ safe_route.py
@@ -1,20 +1,20 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 18 13:01:30 2017
-
-@author: Anju
-"""
-
-
-from flask import Flask
-from flask import send_from_directory
-
+from flask import Flask,render_template
+ 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return send_from_directory('static/html', "index.html")
+    return "Welcome to Safe Route Travel for Walkers!"
+    
+@app.route("/feedback")
+def user_feedback():
+    return render_template('user_feedback.html')
+    #return "feedback form"
+
+
+def submit():
+    
 
 if __name__ == "__main__":
     app.run(port=5000)
